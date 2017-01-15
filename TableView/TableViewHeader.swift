1dfac3a265e6f917f8a596597e5e490f369e5088
KrygTomasz <yaami93@gmail.com>
2017-01-15 20:00:40 +0100

    Tap gesture on header's label
    Add tap gesture - need fix (tap doesn’t work)

--- TableView/TableViewHeader.swift
+++ TableView/TableViewHeader.swift
@@ -11,9 +11,12 @@ import UIKit
 class TableViewHeader: UIView {
 
     @IBOutlet weak var labelName: UILabel!
+    @IBOutlet weak var editableLabelName: UITextField!
     @IBOutlet weak var expandButton: UIButton!
     
     var onHeaderButtonClicked : (()->Void)!
+    
+    var headerExpanded: Bool = false
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -23,19 +26,47 @@ class TableViewHeader: UIView {
     func setView(labelText: String, isExpanded: Bool) {
 
         self.backgroundColor = UIColor.transparent(alpha: 0.5)
-        labelName.text = labelText
+        
+        setLabels(with: labelText)
+        
+        headerExpanded = isExpanded
+        
+        setButtons()
+        
+    }
+    
+    func setLabels(with text: String) {
+        
+        labelName.text = text
         labelName.textColor = Colors.main
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(labelTap))
+        labelName.addGestureRecognizer(tapGesture)
+        //expandButton.addGestureRecognizer(tapGesture)
+        
+        editableLabelName.text = text
+        editableLabelName.isHidden = true
+        
+    }
+    
+    func labelTap() {
+        
+        labelName.isHidden = true
+        editableLabelName.isHidden = false
+        
+    }
+    
+    func setButtons() {
         
         expandButton.setImage(#imageLiteral(resourceName: "expand"), for: [])
         expandButton.tintColor = Colors.main
-        expandButton.addTarget(self, action: #selector(TableViewHeader.buttonClick), for: .touchUpInside)
-        if isExpanded {
+        expandButton.addTarget(self, action: #selector(expandButtonClick), for: .touchUpInside)
+        if headerExpanded {
             expandButton.rotate(by: CGFloat(M_PI))
         }
         
     }
     
-    func buttonClick() {
+    func expandButtonClick() {
         
         onHeaderButtonClicked()
         UIView.animate(withDuration: 0.2, animations: {
