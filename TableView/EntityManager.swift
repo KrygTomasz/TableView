10ec91c652d61beb6267ea4b5bd20f3f3852f648
KrygTomasz <yaami93@gmail.com>
2017-01-23 19:39:10 +0100

    Entity Manager
    Implement function that fetch from database.

---
+++ TableView/EntityManager.swift
@@ -0,0 +1,31 @@
+//
+//  EntityManager.swift
+//  TableView
+//
+//  Created by Kryg Tomasz on 23.01.2017.
+//  Copyright © 2017 Kryg Tomek. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class EntityManager {
+    
+    static func fetch(from entity: String) -> [NSManagedObject] {
+        
+        guard let appDelegate: AppDelegate = UIApplication.shared.delegate as? AppDelegate else { return [] }
+        let context = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: entity)
+        
+        do {
+            let results = try context.execute(fetchRequest)
+            guard let fetchedObjects = results as? [NSManagedObject] else { return [] }
+            return fetchedObjects
+        } catch let error as NSError {
+            print("Couldn't fetch objects due to \(error).")
+            return []
+        }
+
+    }
+    
+}
